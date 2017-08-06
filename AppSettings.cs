71b19624d262fc7a36036a8b49d04062926e9484
hanzworld <github@barrowside.co.nz>
2017-08-06 13:26:28 +1200

    Add configuration settings
    Add config file
    Create matching POCO
    Load on app startup

---
+++ AppSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Office365GmailMigratorChecker
+{
+    public class AppSettings
+    {
+            public int Periods { get; set; }
+            public string PeriodType { get; set; }
+            public int StartYear { get; set; }
+    }
+
+    public class Gmail
+    {
+        public Gmail() { }
+        public string Username { get; set; }
+        public string CredentialPath { get; set; }
+    }
+
+    public class Graph
+    {
+        public Graph() { }
+
+        public string Username { get; set; } = "default";
+        public string Tenant { get; set; }
+        public string ClientId { get; set; }
+        public string Secret { get; set; }
+        public int BatchSize { get; set; } = 100;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace Office365GmailMigratorChecker
 
            var app = serviceProvider.GetService<Application>();
 
+            //Task.Run(() => app.Run()).Wait();
             Task.Run(() => app.Run()).GetAwaiter().GetResult();
 
             Console.ReadKey();
@@ -31,8 +32,23 @@ namespace Office365GmailMigratorChecker
 
         private static void ConfigureServices(IServiceCollection services)
         {
+            //add program configuration
+            IConfigurationRoot configuration = GetConfiguration();
+            services.AddSingleton<IConfigurationRoot>(configuration);
+
+            //separate my configuration settings please
+            services.AddOptions();
+            services.Configure<Gmail>(options => configuration.GetSection("Gmail").Bind(options));
+            services.Configure<Graph>(options => configuration.GetSection("Graph").Bind(options));
+
         }
 
+        private static IConfigurationRoot GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange:true)
+                .Build();
 
 
 
