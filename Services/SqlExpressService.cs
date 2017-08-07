4b6ae1287b613a2236883b55c346d00adece44ce
hanzworld <github@barrowside.co.nz>
2017-08-07 21:45:59 +1200

    Add SqlExpress support to project

---
+++ Services/SqlExpressService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Office365GmailMigratorChecker
+{
+    class SqlExpressService : DbContext
+    {
+            public DbSet<MyMessage> Messages { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Office365GmailMigratorChecker;Trusted_Connection=True;");
+            }
+
+    }
+}
