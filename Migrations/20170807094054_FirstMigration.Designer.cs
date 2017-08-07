858a4a594a2e5fd26ca37d4d498fae626a6cfb5f
hanzworld <github@barrowside.co.nz>
2017-08-07 21:47:38 +1200

    Initial EF Migrations of SQL database

---
+++ Migrations/20170807094054_FirstMigration.Designer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Office365GmailMigratorChecker;
+
+namespace Office365GmailMigratorChecker.Migrations
+{
+    [DbContext(typeof(SqlExpressService))]
+    [Migration("20170807094054_FirstMigration")]
+    partial class FirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Office365GmailMigratorChecker.MyMessage", b =>
+                {
+                    b.Property<string>("Rfc822MsgId");
+
+                    b.Property<string>("GmailId")
+                        .HasColumnType("VARCHAR(255)");
+
+                    b.Property<bool>("IsMigratedToGmail");
+
+                    b.Property<string>("Office365Id")
+                        .IsRequired()
+                        .HasColumnType("VARCHAR(255)");
+
+                    b.Property<DateTime>("SentDateTime");
+
+                    b.Property<string>("Subject")
+                        .IsRequired();
+
+                    b.HasKey("Rfc822MsgId");
+
+                    b.HasIndex("GmailId")
+                        .IsUnique();
+
+                    b.HasIndex("Office365Id")
+                        .IsUnique();
+
+                    b.ToTable("Messages");
+                });
+        }
+    }
+}
---
+++ Migrations/20170807094054_FirstMigration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Office365GmailMigratorChecker.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Rfc822MsgId = table.Column<string>(nullable: false),
+                    GmailId = table.Column<string>(type: "VARCHAR(255)", nullable: true),
+                    IsMigratedToGmail = table.Column<bool>(nullable: false),
+                    Office365Id = table.Column<string>(type: "VARCHAR(255)", nullable: false),
+                    SentDateTime = table.Column<DateTime>(nullable: false),
+                    Subject = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Rfc822MsgId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_GmailId",
+                table: "Messages",
+                column: "GmailId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_Office365Id",
+                table: "Messages",
+                column: "Office365Id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
