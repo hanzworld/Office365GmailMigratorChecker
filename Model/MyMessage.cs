4c565f40e6e38b937407d6f762093e36327baae3
hanzworld <github@barrowside.co.nz>
2017-08-07 21:46:35 +1200

    Add new properties to MyMessage object
    This in preparation to use MyMessage as a POCO straight into EF to save to the database

--- Model/MyMessage.cs
+++ Model/MyMessage.cs
@@ -9,7 +9,12 @@ namespace Office365GmailMigratorChecker
     public class MyMessage
     {
         public Message OutlookMessage { get; set; }
+        public string Rfc822MsgId { get; set; }
         public string GmailId { get; set; }
+        public string Office365Id { get; set; }
         public bool IsMigratedToGmail { get; set; }
+        public string Subject { get; set; }
+        public DateTime SentDateTime { get; set; }
+        public Message OutlookMessage { get; set; }
     }
 }
