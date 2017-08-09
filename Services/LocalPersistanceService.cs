bda08c711f9807cd67319e42853740f2a89d64d6
hanzworld <github@barrowside.co.nz>
2017-08-09 18:17:15 +1200

    BUG: Don't overwrite local stores if we didn't retrieve anything
    This would only happen where something earlier has gone wrong

--- Services/LocalPersistanceService.cs
+++ Services/LocalPersistanceService.cs
@@ -12,6 +12,12 @@ namespace Office365GmailMigratorChecker
 
         public static void PersistResultsToFile(MessageBatch messages, int year, int periods, PeriodType periodtype)
         {
+            //sanity check we don't have
+            if (messages.Messages.Count == 0)
+            {
+                throw new Exception("You're asking me to save an empty file Mr President, that's not supposed to happen");
+            }
+
             using (StreamWriter file = File.CreateText(String.Format(@"Office365DataStore-{0}-{1}{2}.json", year, periods, periodtype)))
             {
                 JsonSerializer serializer = new JsonSerializer();
