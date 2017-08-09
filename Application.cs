ce4dc61e08126ad5fc456ed5b00220e4900f2c48
hanzworld <github@barrowside.co.nz>
2017-08-10 01:28:01 +1200

    Move retrieving Outlook data into method

--- Application.cs
+++ Application.cs
@@ -41,66 +41,10 @@ namespace Office365GmailMigratorChecker
             try
             {
                 // STEP 1: Retrieve a list of messages from Office365 (as the 'original' mail server, it's the source of truth of what should be migrated)
-                
-                //because I'm completely lazy for now, I'm going to store results locally in JSON files - this might bite me later, but at least it'll help me write the app without constant API thrashing
-                if (LocalPersistanceService.LocalFileExists(_settings.StartYear, _settings.Periods, _settings.PeriodLength))
-                {
-                    messageBatch = LocalPersistanceService.ReadResultsFromFile(_settings.StartYear, _settings.Periods, _settings.PeriodLength);
-                }
-                else
-                {
-                    //get them from the API
-                    var outlookData = await _graphService.RetrieveBatch(_settings.StartYear, _settings.Periods);
-                    if (outlookData.Count == 0) return;
-                    //convert them into a data format we actually can use, and persist
-                    //TODO - put this in a proper converter
-                    messageBatch.Messages = outlookData.Select(m => new MyMessage { OutlookMessage = m }).ToList();
-                    LocalPersistanceService.PersistResultsToFile(messageBatch, _settings.StartYear, _settings.Periods, _settings.PeriodLength);
-                }
-               
+                messageBatch = await GetOutlookData(messageBatch);
+
                 //STEP 2: find if these have been imported to Gmail - where the only matching criteria is RFC822 MessageID
-                //TODO given we have to make n calls to Gmail API, one for each message, let's at least batch them shall we?
-
-                //because we may have read out a partially completed file, dont' assume we need to reparse everything!
-                var howManyMessagesAreAlreadyProcessed = messageBatch.ConfirmedMigrationStatus.Count;
-                if (howManyMessagesAreAlreadyProcessed > 1)
-                {
-                    Console.WriteLine("{0} of the loaded message were already processed, quering Gmail API for {1} remaining messages", howManyMessagesAreAlreadyProcessed, messageBatch.Messages.Count - howManyMessagesAreAlreadyProcessed);
-                }
-
-                int i = 0;
-                var errors = new List<string>();
-                foreach (var message in messageBatch.UnconfirmedMigrationStatus)
-                {
-                    try
-                    {
-                        var gmailId = _gmailService.FindMessageByRFC822(message.OutlookMessage.InternetMessageId.Replace("<", "").Replace(">", ""));
-                        bool isInGmail = !String.IsNullOrWhiteSpace(gmailId);
-
-                        message.IsMigratedToGmail = isInGmail;
-
-                        if (isInGmail) { 
-                            message.GmailId = gmailId;
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        errors.Add(String.Format("Couldn't retrieve id {0} from Gmail (date: {1}, subject: {2}). Error was {3}", message.OutlookMessage.InternetMessageId, message.OutlookMessage.SentDateTime, message.OutlookMessage.Subject, e.Message));
-                    }
-                    finally
-                    {
-                        i++;
-                        if (i % 100 == 0)
-                        {
-                            Console.WriteLine("Retriving Gmail information for messages {0}00 and onwards", i / 100);
-                        }
-                    }
-
-                    
-                }
-                Console.WriteLine(messageBatch);
-
-                LocalPersistanceService.PersistResultsToFile(messageBatch, _settings.StartYear, _settings.Periods, _settings.PeriodLength);
+                messageBatch = MatchToGmailData(messageBatch);
 
                 // STEP 3: Where we have messages which are not migrated, we need to store those so it's queryable over and over
                 _dataStoreService.WriteToDb(messageBatch.NotMigratedMessages);
@@ -120,7 +64,77 @@ namespace Office365GmailMigratorChecker
                 LocalPersistanceService.PersistResultsToFile(messageBatch, _settings.StartYear, _settings.Periods, _settings.PeriodLength);
             }
         }
+        
+        private MessageBatch MatchToGmailData(MessageBatch messageBatch)
+        {
+            //TODO given we have to make n calls to Gmail API, one for each message, let's at least batch them shall we?
 
-       
+            //because we may have read out a partially completed file, dont' assume we need to reparse everything!
+            var howManyMessagesAreAlreadyProcessed = messageBatch.ConfirmedMigrationStatus.Count;
+            if (howManyMessagesAreAlreadyProcessed > 1)
+            {
+                Console.WriteLine("{0} of the loaded message were already processed, quering Gmail API for {1} remaining messages", howManyMessagesAreAlreadyProcessed, messageBatch.Messages.Count - howManyMessagesAreAlreadyProcessed);
+            }
+
+            int i = 0;
+            var errors = new List<string>();
+            foreach (var message in messageBatch.UnconfirmedMigrationStatus)
+            {
+                try
+                {
+                    var gmailId = _gmailService.FindMessageByRFC822(message.OutlookMessage.InternetMessageId.Replace("<", "").Replace(">", ""));
+                    bool isInGmail = !String.IsNullOrWhiteSpace(gmailId);
+
+                    message.IsMigratedToGmail = isInGmail;
+
+                    if (isInGmail)
+                    {
+                        message.GmailId = gmailId;
+                    }
+                }
+                catch (Exception e)
+                {
+                    errors.Add(String.Format("Couldn't retrieve id {0} from Gmail (date: {1}, subject: {2}). Error was {3}", message.OutlookMessage.InternetMessageId, message.OutlookMessage.SentDateTime, message.OutlookMessage.Subject, e.Message));
+                }
+                finally
+                {
+                    i++;
+                    if (i % 100 == 0)
+                    {
+                        Console.WriteLine("Retriving Gmail information for messages {0}00 and onwards", i / 100);
+                    }
+                }
+
+
+            }
+            Console.WriteLine(messageBatch);
+
+            LocalPersistanceService.PersistResultsToFile(messageBatch, _settings.StartYear, _settings.Periods, _settings.PeriodLength);
+        }
+
+        async Task<MessageBatch> GetOutlookData(MessageBatch messageBatch)
+        {
+            //because I'm completely lazy for now, I'm going to store results locally in JSON files - this might bite me later, but at least it'll help me write the app without constant API thrashing
+            if (LocalPersistanceService.LocalFileExists(_settings.StartYear, _settings.Periods, _settings.PeriodLength))
+            {
+                messageBatch = LocalPersistanceService.ReadResultsFromFile(_settings.StartYear, _settings.Periods, _settings.PeriodLength);
+            }
+            else
+            {
+                //get them from the API
+                var outlookData = await _graphService.RetrieveBatch(_settings.StartYear, _settings.Periods);
+                if (outlookData.Count == 0) return null;
+                //convert them into a data format we actually can use, and persist
+
+                //TODO - put this in a proper converter
+                messageBatch.Messages = outlookData.Select(m => new MyMessage { OutlookMessage = m }).ToList();
+
+                LocalPersistanceService.PersistResultsToFile(messageBatch, _settings.StartYear, _settings.Periods, _settings.PeriodLength);
+            }
+            return messageBatch;
+
+        }
+
+
     }
 }
