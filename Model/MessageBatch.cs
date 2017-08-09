e172875ac4a8fa094c21a561955b5ce8e86261d6
hanzworld <github@barrowside.co.nz>
2017-08-09 18:03:44 +1200

    Create a MessageBatch object
    Wraps around our list of messages, provided helper methods and
    properties and ability to print stats. This wraps up much of the logic I
    was repeatedly doing to the list of messages to find messages that fit
    certain criteria

---
+++ Model/MessageBatch.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Office365GmailMigratorChecker.Model
+{
+    class MessageBatch
+    {
+        public MessageBatch()
+        {
+        }        
+
+        public List<MyMessage> Messages { get; set; }
+
+        public List<MyMessage> ConfirmedMigrationStatus { get {
+                return Messages.Where(m => m.IsMigratedToGmail.HasValue).ToList();
+            } }
+
+        public List<MyMessage> UnconfirmedMigrationStatus { get {
+               return Messages.Where(m => !m.IsMigratedToGmail.HasValue).ToList();
+            } }
+
+        public List<MyMessage> MigratedMessages
+        {
+            get
+            {
+                return Messages.Where(m => m.IsMigratedToGmail.GetValueOrDefault()).ToList();
+            }
+        }
+
+        public List<MyMessage> NotMigratedMessages
+        {
+            get
+            {
+                return Messages.Where(m => m.IsMigratedToGmail.HasValue && !m.IsMigratedToGmail.Value).ToList();
+            }
+        }
+    }
+}
